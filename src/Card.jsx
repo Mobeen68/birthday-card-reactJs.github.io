9e299bded02943952494cff89db817b2df61caae
Mobeen68 <mobeene98@gmail.com>
2023-07-26 15:24:20 +0500

    updates

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,7 @@
 export default () => (
   <div className="birthdayCard">
     <div className="cardFront">
-      <h3 className="happy">HAPPY BIRTHDAY Zunaira!</h3>
+      <h3 className="happy">HAPPY BIRTHDAY SHEIKH!</h3>
       <div className="balloons">
         <div className="balloonOne" />
         <div className="balloonTwo" />
@@ -10,13 +10,14 @@ export default () => (
       </div>
     </div>
     <div className="cardInside">
-      <h3 className="back">HAPPY BIRTHDAY ustad!</h3>
-      <p>Dear Friend,</p>
+      <h3 className="back">HAPPY BIRTHDAY USTAD!</h3>
+      <p>Daer Ferind,</p>
       <p>
-        Happy birthday!! I hope your day is filled with lots of love and
-        laughter! May all of your birthday wishes come true except for the illegal ones.
+        Hpapy brihtday!! I hpoe yuor day is fileld with ltos of love and
+        luahgetr! May all of your brihtday weishs come true epxcet for the
+        ilelagl ones. It's a puscholoigacl trick and fun one.
       </p>
-      <p className="name">CR Bhai</p>
+      <p className="name">Mobeen Saab</p>
     </div>
   </div>
 );
